3c5ccce8ab6678a54d9e9df3fc49f8f900afb817
Matthew Wright <matt.wright53911@gmail.com>
2019-10-24 14:50:04 -0500

    Working filter
    Filter is working for the decades and all. Added filter status as well as an @Input attribute.

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {OmdbService} from "./omdb.service";
 import {Movie} from "./movie.model";
+import _ from 'lodash';
 
 @Component({
     selector: 'movie-list',
@@ -8,24 +9,28 @@ import {Movie} from "./movie.model";
     styleUrls: ['./movie-list.component.sass']
 })
 export class MovieListComponent implements OnInit {
-    headerTitle:string;
+    @Input() headerTitle: string;
     footerCopyright: string;
     masterMovieList: Movie[];
+    filterName: string = 'All';
     movies: IMovieDetails[] = [];
+    loadedMovies: IMovieDetails[] = [];
     localPosterImageUrl: string = '/assets/images/posters/';
 
-    constructor(private _ombdService: OmdbService) {
+    constructor(private _ombdService: OmdbService, private elm: ElementRef) {
     }
 
     ngOnInit() {
-        this.headerTitle = 'Holy Bunions Batman! - Batman Time Machine';
         this.footerCopyright ='Copyright 2019. All images and logos belong to their respective owners.';
         this._ombdService.getAllMovies()
           .subscribe((data: any) => {
             this.masterMovieList = data.Search;
 
             for(let i = 0, il = this.masterMovieList.length; i < il; i++) {
-              this._ombdService.getMovie(this.masterMovieList[i].imdbID).subscribe((movie: IMovieDetails) => this.movies = [...this.movies, movie])
+              this._ombdService.getMovie(this.masterMovieList[i].imdbID).subscribe((movie: IMovieDetails) => {
+                  this.movies = [...this.movies, movie];
+                  this.loadedMovies = this.movies;
+              });
             }
           });
     }
@@ -33,6 +38,23 @@ export class MovieListComponent implements OnInit {
     getNewMoviePosterUrl(url: string) {
         const imgFileName = url.replace('https://m.media-amazon.com/images/M/', '');
         return `${this.localPosterImageUrl}${imgFileName}`;
+    }
+
+    filterMoviesByDecade(decade: string) {
+        if(decade) {
+            this.filterName = `${decade}'s`;
+            this.loadedMovies = _.filter(this.movies, ({Released}) => {
+                const releaseYear = Released.substr(-4, 4);
+                if(decade.substr(0,1) === '2') {
+                    return releaseYear.substr(0,1) === '2';
+                } else {
+                    return decade.substr(0,3) === releaseYear.substr(0,3);
+                }
+            });
+        } else {
+            this.filterName = 'All';
+            this.loadedMovies = this.movies;
+        }
     }
 }
 export interface IMovie {
